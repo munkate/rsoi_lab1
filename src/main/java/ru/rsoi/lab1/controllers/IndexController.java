8433396b0700a572fadd151ad14f7624a1b7f1d1
munkate <Munekaterina@yahoo.com>
2018-09-30 10:41:42 -0700

    first version

---
+++ src/main/java/ru/rsoi/lab1/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package ru.rsoi.lab1.controllers;
+ import org.springframework.stereotype.Controller;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RequestMethod;
+ import org.springframework.web.bind.annotation.RequestParam;
+ import org.springframework.web.bind.annotation.ResponseBody;
+ import javax.servlet.http.HttpServletRequest;
+ import javax.validation.constraints.NotNull;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping(value = "/", method={RequestMethod.GET})
+    @ResponseBody
+    String hello(HttpServletRequest request, @RequestParam(value = "name", required = false) String name) {
+        if (name==null)
+            return "Hello world!";
+        else
+            return "Hello "+ request.getParameter("name")+"!";
+    }
+
+}
